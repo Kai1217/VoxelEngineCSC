05245290ae0bc8db45bdff412732bd9dbb6c83da
Kai Matolat <kaimatolat@hotmail.com>
2019-04-22 02:27:51 +1000

    Added Render.cs, need to read more on the OpenTK Graphics API.

---
+++ Render.cs
@@ -0,0 +1,58 @@
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using OpenTK.Graphics;
+using System.IO;
+
+namespace VoxelEngineCSC
+{
+    class Render
+    {
+        public void Renderer()
+        {
+            /* 
+            GL.ClearColor();
+            GL.Enable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.PolygonOffsetFill);
+            // TODO: Other Variables (mx, my, mz) and other logic
+            int px = Math.Floor(Convert.ToFloat(prevpos.x));
+            int py = Math.Floor(Convert.ToFloat(prevpos.y));
+            int pz = Math.Floor(Convert.ToFloat(prevpos.z));
+            // FILLER
+
+            float[, ] Box = new float[24, 4] {
+                {bx + 0, by + 0, bz + 0, 14},
+		        {bx + 1, by + 0, bz + 0, 14},
+		        {bx + 0, by + 1, bz + 0, 14},
+		        {bx + 1, by + 1, bz + 0, 14},
+                {bx + 0, by + 0, bz + 1, 14},
+                {bx + 1, by + 0, bz + 1, 14},
+                {bx + 0, by + 1, bz + 1, 14},
+                {bx + 1, by + 1, bz + 1, 14},
+
+                {bx + 0, by + 0, bz + 0, 14},
+                {bx + 0, by + 1, bz + 0, 14},
+                {bx + 1, by + 0, bz + 0, 14},
+                {bx + 1, by + 1, bz + 0, 14},
+                {bx + 0, by + 0, bz + 1, 14},
+                {bx + 0, by + 1, bz + 1, 14},
+                {bx + 1, by + 0, bz + 1, 14},
+                {bx + 1, by + 1, bz + 1, 14},
+
+                {bx + 0, by + 0, bz + 0, 14},
+                {bx + 0, by + 0, bz + 1, 14},
+                {bx + 1, by + 0, bz + 0, 14},
+                {bx + 1, by + 0, bz + 1, 14},
+                {bx + 0, by + 1, bz + 0, 14},
+                {bx + 0, by + 1, bz + 1, 14},
+                {bx + 1, by + 1, bz + 0, 14},
+                {bx + 1, by + 1, bz + 1, 14},
+            };
+            */
+        }
+    }
+}
